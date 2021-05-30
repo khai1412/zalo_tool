be97bd2164b07eafd8052b7dd953efa446be037a
khai1412 <khai1412@gmail.com>
2021-05-30 22:05:12 +0700

    new

---
+++ Tool_zalo/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Tool_zalo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtg_NoiDung = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_NoiDung)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtg_NoiDung
+            // 
+            this.dtg_NoiDung.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_NoiDung.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dtg_NoiDung.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dtg_NoiDung.Location = new System.Drawing.Point(0, 0);
+            this.dtg_NoiDung.Margin = new System.Windows.Forms.Padding(2);
+            this.dtg_NoiDung.Name = "dtg_NoiDung";
+            this.dtg_NoiDung.RowHeadersWidth = 82;
+            this.dtg_NoiDung.RowTemplate.Height = 24;
+            this.dtg_NoiDung.Size = new System.Drawing.Size(864, 230);
+            this.dtg_NoiDung.TabIndex = 0;
+            this.dtg_NoiDung.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtg_NoiDung_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "STT";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Số điện thoại";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 200;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nội dung tin nhắn";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 350;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Kết bạn";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 90;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Nhắn tin";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 90;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.button1.Location = new System.Drawing.Point(0, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(864, 81);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 325);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtg_NoiDung);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_NoiDung)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtg_NoiDung;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tool_zalo/Form1.cs
@@ -0,0 +1,93 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_zalo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void KetBan_And_Nhantin(IWebDriver browser)
+        {
+            for(int i = 0; i < dtg_NoiDung.RowCount-1; i++)
+            {
+                dtg_NoiDung.Rows.Add(i+1);
+                try
+                {
+                    browser.FindElement(By.Id("contact-search-input")).SendKeys(dtg_NoiDung.Rows[i].Cells[1].Value.ToString());
+                    Thread.Sleep(4000);
+                    if ((bool)dtg_NoiDung.Rows[i].Cells[3].Value == true)
+                    {
+                        try
+                        {
+                            browser.FindElement(By.XPath("/html/body/div/div/div[2]/nav/div[2]/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/div/div[2]")).Click();
+                            Thread.Sleep(3000);
+                            browser.FindElement(By.XPath("/html/body/div/div/div[2]/main/div/article/div[2]/div[1]/div[1]/div/div")).Click();
+                            Thread.Sleep(2000);
+                            browser.FindElement(By.XPath("/html/body/div/div/div[3]/div/div/div[2]/div/div/div[5]/div[2]")).Click();
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                    if ((bool)dtg_NoiDung.Rows[i].Cells[4].Value == true)
+                    {
+
+                        browser.FindElement(By.Id("input_line_0")).SendKeys(dtg_NoiDung.Rows[i].Cells[2].Value.ToString());
+                        Thread.Sleep(2000);
+                        browser.FindElement(By.XPath("/html/body/div/div/div[2]/main/div/article/div[4]/div[3]/div/div/div/div/div[2]/div[5]/div")).Click();
+                    }
+                }
+                catch
+                {
+
+                }
+                Thread.Sleep(2000);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo("Profile");
+            List<DirectoryInfo> NameOfProfile = dir.GetDirectories().ToList();
+            IWebDriver browser;
+            ChromeDriverService cService = ChromeDriverService.CreateDefaultService();
+            cService.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--user-data-dir=" + NameOfProfile[0].FullName);
+            options.AddArguments("--disable-notifications");
+            options.AddArguments("--disable-infobars");
+            options.AddArguments("--disable-popup-blocking");
+            options.AddAdditionalCapability("useAutomationExtension", false);
+            options.AddExcludedArgument("enable-automation");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-dev-shm-usage");
+            browser = new ChromeDriver(cService, options);
+            browser.Navigate().GoToUrl("https://chat.zalo.me/");
+            Thread.Sleep(2000);
+            KetBan_And_Nhantin(browser);
+            
+
+        }
+
+        private void dtg_NoiDung_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
